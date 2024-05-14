a93a175e89fdd3029c2b423efd08c5a34f678bed
son7877 <hongbeom7877@naver.com>
2024-05-14 10:37:28 +0900

    map api

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -12,6 +12,7 @@ const MapContainer = () => {
   const markerRef = useRef(null);
 
   useEffect(() => {
+    // 초기 지도 생성
     const initialLat = 37.48645289999874;
     const initialLng = 127.02067890000285;
 
@@ -20,6 +21,7 @@ const MapContainer = () => {
       zoom: 18,
     });
 
+    // 초기 마커 생성
     const marker = new window.naver.maps.Marker({
       position: new window.naver.maps.LatLng(initialLat, initialLng),
       map: mapInstance,
@@ -35,6 +37,7 @@ const MapContainer = () => {
     if (!address) return;
 
     window.naver.maps.Service.geocode(
+      // 도로명 주소나 지번 주소 둘 다 가능
       { query: address },
       (status, response) => {
         if (
@@ -44,13 +47,15 @@ const MapContainer = () => {
           return alert("주소를 찾을 수 없습니다.");
         }
 
-        const result = response.v2.addresses[0]; // API 응답 구조에 따라 접근 수정
+        // 주소 검색 결과
+        const result = response.v2.addresses[0];
+        // 주소 검색 후 새로운 좌표 생성
         const newCoords = new window.naver.maps.LatLng(result.y, result.x);
 
-        // 지도의 중심을 변경합니다.
+        // 지도의 중심을 변경
         map.setCenter(newCoords);
 
-        // 마커 위치를 업데이트합니다.
+        // 마커 위치를 업데이트
         markerRef.current.setPosition(newCoords);
       }
     );
@@ -58,14 +63,14 @@ const MapContainer = () => {
 
   return (
     <>
-      <div>
+      <div className="size-full">
         <button onClick={login}>로그인</button>
       </div>
       <div>
         <input id="address" type="text" placeholder="주소를 입력하세요" />
         <button onClick={searchAddressAndMoveMap}>검색</button>
-        <div id="map" style={{ width: "100%", height: "700px" }}></div>
       </div>
+      <div id="map" className="w-full h-96"></div>
     </>
   );
 };
